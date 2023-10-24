44e8479ca0af626476773e1ae13b9b150b326056
threadexception <hannes.gaumann@outlook.de>
2023-10-24 14:10:05 +0200

    Cleanup some impls

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use std::mem::transmute;
+
 /// a short, branchless algorithm that is eqivalent to
 /// if num > 0:
 ///    ret 1
@@ -34,12 +36,12 @@ fn greater_zero_ret_one_2(num: usize) -> usize {
 /// else:
 ///    ret 0
 #[inline]
-fn greater_zero_ret_one_3(num: usize) -> usize {
+const fn greater_zero_ret_one_3(num: usize) -> usize {
+    const MSB_OFF: usize = (usize::BITS - 1) as usize;
+
     // if num is 0, identity will have a value of 0 as all bits are 0, for other values, this will overflow.
     let identity = 0_usize - num;
-    let msb_off = (u32::BITS - 1) as usize;
-    let msb = 1 << msb_off;
-    (identity & msb) >> msb_off
+    identity >> MSB_OFF
 }
 
 /// a short, branchless algorithm that is eqivalent to
@@ -48,7 +50,7 @@ fn greater_zero_ret_one_3(num: usize) -> usize {
 /// else:
 ///    ret 0
 #[inline]
-fn greater_zero_ret_one_4(num: usize) -> usize {
+const fn greater_zero_ret_one_4(num: usize) -> usize {
     // if num is 0, identity will have a value of 0 as all bits are 0, for other values, this will overflow.
     let identity = 0_usize.overflowing_sub(num);
     unsafe { transmute::<bool, u8>(identity.1) as usize }
